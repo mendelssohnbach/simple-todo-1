741c55649dcbfebe7e9e3a9ba91b908dd5463f7b
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2024-11-30 05:12:22 +0900

    ✨ TodoListに機能を追加する

--- src/todo/TodoList.tsx
+++ src/todo/TodoList.tsx
@@ -9,6 +9,8 @@ import TodoItem from './TotoItem';
 interface Todo {
   id: number;
   text: string;
+  isEditing: boolean;
+  isCompleted: boolean;
 }
 
 export default function TodoList() {
@@ -16,14 +18,38 @@ export default function TodoList() {
   const [input, setInput] = useState('');
 
   const addTodo = () => {
-    setTodos([...todos, { id: Date.now(), text: input }]);
+    setTodos([...todos, { id: Date.now(), text: input, isEditing: false, isCompleted: false }]);
     setInput('');
+  };
+
+  const toggleComplete = (id: number) => {
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo))
+    );
+  };
+
+  const startEditing = (id: number) => {
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, isEditing: true } : todo)));
+  };
+
+  const cancelEditing = (id: number) => {
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, isEditing: false } : todo)));
+  };
+
+  const saveEdit = (id: number, newText: string) => {
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, text: newText, isEditing: false } : todo))
+    );
   };
 
   const todoChildren = todos.map((todo) => (
     <TodoItem
       key={todo.id}
       todo={todo}
+      toggleComplete={toggleComplete}
+      startEditing={startEditing}
+      cancelEditing={cancelEditing}
+      saveEdit={saveEdit}
     />
   ));
 
